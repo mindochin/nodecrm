07fb47da599598cb3432a6b53c81fb4953801dbc
Mindochin <mindochin@yandex.ru>
2019-12-06 14:18:58 +0500

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://vlad:q3dmm6@cluster0-svrc0.gcp.mongodb.net/nodecrm',
+  mongoURI: 'mongodb+srv://vlad:123456@cluster0-svrc0.gcp.mongodb.net/nodecrm',
   jwt: 'dev-jwt'
 }
